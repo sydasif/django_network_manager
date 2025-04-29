6997e7ddc377422ac31e5c4d704255cf59bd46fa
Syed Asif <sydasif78@gmail.com>
2025-04-29 15:00:19 +0500

    created a home page

--- network_manager/urls.py
+++ network_manager/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("tools/", include("netmiko_tools.urls")),
+    path("", lambda request: redirect("tools/"), name="home"),
 ]
